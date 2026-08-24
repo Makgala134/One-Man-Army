1a18e790102d906e4665c7783361119a5fafc1fa
Makgala134 <realebohamahlatsi1@gmail.com>
2026-08-24 20:06:29 +0200

    fix errors

--- Assets/Scripts/Interactable/Door.cs
+++ Assets/Scripts/Interactable/Door.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Door : MonoBehaviour
+public class Door : InteractableNameText
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
