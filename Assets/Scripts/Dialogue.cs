ae0b8ab1c988020cfc98bb511bd339bf38984c2e
Oratile-14 <oratilemashinini14@gmail.com>
2026-09-22 19:22:03 +0200

    Reapply "Dialogue"
    This reverts commit 56b5b1019d667ade1fcc3f8569f6904646a15965.

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -8,6 +8,8 @@ public class Dialogue : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
     public string[] lines;
+    public int roomline;
+
     public float textSpeed;
 
     private int index;
@@ -44,6 +46,7 @@ public class Dialogue : MonoBehaviour
         StartCoroutine(TypeLine());
 
     }
+
 
     IEnumerator TypeLine()
     {
