c5ad2dcfff3baa41c1403aac2371fed45d64c376
Oratile-14 <oratilemashinini14@gmail.com>
2026-09-22 22:27:13 +0200

    Revert "Merge branch 'Lebo-copy' into Main-Copy-(2)"
    This reverts commit bfb14e70aac4db6e20de584f7003795e2117ddbe, reversing
    changes made to 81b1b64fc71f434b2b5c759d0320eb9b12a3039b.

--- Assets/Menu Main Scripts/Menu Main Scripts/Timer.cs
+++ Assets/Menu Main Scripts/Menu Main Scripts/Timer.cs
@@ -8,6 +8,7 @@ public class Timer : MonoBehaviour
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime ;
 
+
     void Update()
     {
         if (remainingTime > 0)
@@ -17,13 +18,11 @@ public class Timer : MonoBehaviour
         else if (remainingTime < 0)
         {
             remainingTime = 0;
-            //GameOver();
+            // GameOver();
             timerText.color = Color.red;
         }
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
-
- 
 }
